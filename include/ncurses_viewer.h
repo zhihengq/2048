81ced1840183a3cbb3d626ce871634e2bac2abd3
Frank Qin <qinzhiheng24@gmail.com>
2018-09-05 11:09:36 -0400

    Fix documentation for NcursesViewer

--- include/ncurses_viewer.h
+++ include/ncurses_viewer.h
@@ -7,6 +7,9 @@
 
 namespace _2048 {
 
+/**
+ * A Ncurses based CLI viewer.
+ */
 class NcursesViewer : public Viewer {
  public:
     static NcursesViewer viewer;            /**< The NcursesViewer instance */
