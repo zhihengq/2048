a873b243519a2696f1082461eb2a634f73f2f537
Frank Qin <qinzhiheng24@gmail.com>
2018-09-05 18:26:45 -0400

    Add 2048_ncurses application

---
+++ src/2048_ncurses.cc
@@ -0,0 +1,49 @@
+#include <cstdint>
+#include <ncurses.h>
+#include "game.h"
+#include "ncurses_controller.h"
+#include "random_generator.h"
+
+inline void init(_2048::Game &game) {
+    game.Reset(_2048::GameState(4, 4));
+    for (uint32_t i = 0; i < 2; i++)
+        game.Generate();
+}
+
+// return true if the user choose to quit
+// return false otherwise
+bool ProcessMenu(int key, _2048::Game &game) {
+    switch (key) {
+        case 'q':
+            return true;
+        case 'r':
+            init(game);
+            return false;
+        default:
+            return false;
+    }
+}
+
+int main() {
+    _2048::RandomGenerator generator;
+    _2048::NcursesController &control = _2048::NcursesController::instance();
+    _2048::Game game(&control, &generator, &control);
+
+    init(game);
+
+    // take turns
+    bool player_turn = true;
+    while (true) {
+        if (player_turn) {
+            if (game.Play())
+                player_turn = false;
+            else if (ProcessMenu(control.GetLastKey(), game))
+                return 0;
+        } else {
+            game.Generate();
+            player_turn = true;
+        }
+    }
+
+    return 0;
+}
